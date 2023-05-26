7ddb79248f3453b677947abe120b6650b23aedb3
Steve Oman <stephen_oman145@yahoo.com>
2023-05-25 22:33:40 -0700

    Issue #1 - Created an exception class to handle errors thrown when input validation fails.

---
+++ app/Exceptions/CustomValidationException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+
+class CustomValidationException extends Exception
+{
+  /**
+   * Used to throw error when input validation fails.
+   * @param mixed $message Detailed message retrieved from catch block ValidationException
+   * @param string $errorText Pop-up window main text
+   * @param string $errorDescription Details about why an error is thrown
+   */
+  public function __construct($message, $errorText, $errorDescription) {
+    response()->json([
+      'errorText' => $errorText,
+      'errorDescription' => $errorDescription,
+      'errorDetails' => $message
+    ])->send();
+    exit;
+  }
+}
