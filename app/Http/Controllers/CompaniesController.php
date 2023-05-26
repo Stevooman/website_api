d4b8a666f6811a9cb09062a268d82989758d5a83
Steve Oman <stephen_oman145@yahoo.com>
2023-05-25 22:38:20 -0700

    Issue #1 - Finished logic for PUT call - Created a getUpdateArray() function that is used to retrieve input data and pass that data into Laravel's ->update() method.

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\CustomValidationException;
 use App\Models\Company;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class CompaniesController extends Controller
 {
@@ -23,7 +23,7 @@ class CompaniesController extends Controller
 
 
   public function showOne(Request $request) {
-
+    
     $company = Company::find($request->companyId);
     return response()->json($company);
   }
@@ -55,10 +55,64 @@ class CompaniesController extends Controller
 
   public function update(Request $request) {
 
+    $companyId = $request->companyId;
+    $companyName = $request->companyName;
+    $companyAddr = $request->companyAddr;
+    $active = $request->active;
+
+    try {
+      $validatedArr = $this->validate($request, [
+      'companyName' => 'nullable|string|max:30',
+      'companyAddr' => 'nullable|string|max:100',
+      'active' => 'nullable|integer|min:0|max:1',
+      ]);
+    } catch (ValidationException $e) {
+      $messages = $e->validator->errors()->getMessages();
+      throw new CustomValidationException(
+        $messages,
+        'Input Error',
+        'One or more fields did not pass validation.'
+      );
+    }
+
+    // Build data array to update the model with
+    $updateData = $this->getUpdateArray($companyName, $companyAddr, $active);
+
+    try {
+      $updated = Company::where('companyId', $companyId)->update($updateData);
+
+    } catch (QueryException $e) {
+      throw new \Exception($e->getMessage());
+    }
+
+    return response()->json(['updated' => $updated]);
   }
 
 
   public function delete(Request $request) {
 
+  }
+
+
+  /**
+   * Build an array of parameters used to update the database.
+   * @param mixed $name The company name
+   * @param mixed $address The company address
+   * @param mixed $active 1 = active, 2 = not active
+   * @return array
+   */
+  private function getUpdateArray($name, $address, $active) {
+    $updateData = [];
+    if ($name) {
+      $updateData['companyName'] = $name;
+    }
+    if ($address) {
+      $updateData['companyAddr'] = $address;
+    }
+    if ($active != null) {
+      $updateData['active'] = $active;
+    }
+
+    return $updateData;
   }
 }
