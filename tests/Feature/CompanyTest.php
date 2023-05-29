5b5a7777cb1e4287c66eefabe8d6c4d6450ed5da
Steve Oman <stephen_oman145@yahoo.com>
2023-05-29 12:26:43 -0700

    Issue #1 - Add all logic to CompanyTest.php - all major methods tested but not all lines are covered yet.

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Http\Response;
+use App\Models\Company;
 
 class CompanyTest extends TestCase
 {
@@ -42,6 +43,76 @@ class CompanyTest extends TestCase
         '*' => [
           'id', 'companyName', 'companyAddr', 'active', 'created_at', 'updated_at', 'deleted_at'
         ]
+        ]);
+
+    $response = $this->get('api/v1/activeCompanies');
+    $json = $response->json();
+    foreach ($json as $j) {
+      $this->assertSame(1, $j['active']);
+    }
+  }
+
+
+
+  public function testCompanyIsCreatedSuccessfully(): void 
+  {
+    $inputData = [
+      'companyName' => $this->faker->name,
+      'companyAddr' => $this->faker->address(),
+      'active' => 1
+    ];
+
+    $this->post('api/v1/companies', $inputData)
+      ->assertStatus(Response::HTTP_CREATED)
+      ->assertExactJson([
+        'status' => 'success'
       ]);
+
+    $this->assertDatabaseHas('companies', $inputData);
+  }
+
+
+
+  public function testCompanyIsUpdatedSuccessfully(): void 
+  {
+    $company = Company::create([
+      'companyName' => $this->faker->name,
+      'companyAddr' => $this->faker->address
+    ]);
+
+    $inputData = [
+      'companyName' => $this->faker->name,
+      'companyAddr' => $this->faker->address
+    ];
+
+    $this->put('api/v1/companies/' . $company->id, $inputData)
+      ->assertStatus(Response::HTTP_OK)
+      ->assertExactJson([
+        'updated' => 1
+      ]);
+
+    $this->assertDatabaseHas('companies', $inputData);
+  }
+
+
+
+  public function testCompanyIsSoftDeletedSuccessfully(): void 
+  {
+    $company = Company::create([
+      'companyName' => $this->faker->name,
+      'companyAddr' => $this->faker->address
+    ]);
+
+    $this->delete("api/v1/companies/$company->id")
+      ->assertStatus(Response::HTTP_OK)
+      ->assertExactJson([
+        'deleted' => 1
+      ]);
+
+    $this->assertSoftDeleted('companies', [
+      'id' => $company->id, 
+      'companyName' => $company->companyName,
+      'companyAddr' => $company->companyAddr
+    ]);
   }
 }
