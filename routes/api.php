5574663053da3d1cb47f3de654b48dc1dc77255b
Steve Oman <stephen_oman145@yahoo.com>
2023-06-10 22:04:41 -0700

    Issue #11 - Complete routes for the System controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompaniesController;
+use App\Http\Controllers\SystemsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Companies
 Route::controller(CompaniesController::class)->group(function () {
   Route::get('companies', 'index')->name('companies.index');
   Route::get('companies/{companyId}', 'showOne')->name('companies.showOne');
@@ -26,4 +28,14 @@ Route::controller(CompaniesController::class)->group(function () {
   Route::post('companies', 'create')->name('companies.create');
   Route::put('companies/{companyId}', 'update')->name('companies.update');
   Route::delete('companies/{companyId}', 'delete')->name('companies.delete');
+});
+
+// Systems
+Route::controller(SystemsController::class)->group(function () {
+  Route::get('systems', 'index')->name('systems.index');
+  Route::get('systems/{id}', 'showOne')->name('systems.showOne');
+  Route::get('systemReleases', 'showDateRange')->name('systems.showDateRange');
+  Route::post('systems', 'create')->name('systems.create');
+  Route::put('systems/{id}', 'update')->name('systems.update');
+  Route::delete('systems/{id}', 'delete')->name('systems.delete');
 });
