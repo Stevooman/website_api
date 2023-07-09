287563de4ea3be0525283c437364b65996e4bf19
Steve Oman <stephen_oman145@yahoo.com>
2023-07-09 16:15:22 -0700

    Issue #17 - Add  array to ZeldaGame model.

--- app/Models/ZeldaGame.php
+++ app/Models/ZeldaGame.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ZeldaGame extends Model
 {
-	use HasFactory;
+	use HasFactory, SoftDeletes;
+
+	private const SYSTEM_ID = 'systemId';
+	private const TITLE = 'title';
+
+	/**
+   * The columns that are mass assignable within the table.
+   *
+   * @var array
+   */
+  protected $fillable = [self::SYSTEM_ID, self::TITLE];
 }
